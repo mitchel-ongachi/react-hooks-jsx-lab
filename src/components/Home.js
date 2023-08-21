b2355d5a13f8d68043f61b94064bd3c39f5cbfd5
mitchel buyengo <mitchelongachi63@gmail.com>
2023-08-21 14:32:37 +0300

    work complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,17 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const headingStyle = {
+    color: "firebrick",
+  };
+
+  return (
+    <div id="home">
+      <h1 style={headingStyle}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
